b00aff833353a1dafd227d447130858942af68ad
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2023-07-25 07:08:00 -0700

    NavBar Fully Responsive

--- public/index.js
+++ public/index.js
@@ -1,34 +1,64 @@
 const burger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 const navbar = document.querySelector('header');
+const logo = document.querySelector('#logo');
+const offsetHeight = document.querySelector('header').offsetHeight;
 
 burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
-        navbar.classList.add('bg-black');
+        navbar.classList.add('bg-white');
+        logo.classList.remove('hidden');
+        logo.classList.add('inline-block'); 
+        navbar.classList.remove('text-primary-white');
+        navbar.classList.add('text-primary-black');
+
+
        
     } else {
+        if (window.pageYOffset > offsetHeight) {
+            navbar.classList.add('bg-white');
+            navbar.classList.remove('text-primary-white');
+            navbar.classList.add('text-primary-black');
+            
+        } else {
+            navbar.classList.remove('bg-white');
+            navbar.classList.remove('text-primary-black');
+            navbar.classList.add('text-primary-white');
+            logo.classList.remove('inline-block');
+            logo.classList.add('hidden');
+            
+            
+        }
         menu.classList.add('hidden');
-        navbar.classList.remove('bg-black');
-       
+         
+              
+        
+        
     }
+    
+    
 });
 
 window.addEventListener('scroll', function() {
-   
-    const offsetHeight = document.querySelector('header').offsetHeight;
 
     if (window.pageYOffset > offsetHeight) {
-       
+        logo.classList.remove('hidden');
+        logo.classList.add('inline-block');
+
         navbar.classList.add('bg-white');
         navbar.classList.remove('text-primary-white');
         navbar.classList.add('text-primary-black');
-    } else {
-        navbar.classList.remove('bg-white');
-        navbar.classList.remove('text-primary-black');
-        navbar.classList.add('text-primary-white');
-
-
-       
+    } 
+    else{
+        
+        if (menu.classList.contains('hidden')){ 
+            navbar.classList.remove('bg-white');
+            navbar.classList.remove('text-primary-black');
+            navbar.classList.add('text-primary-white');
+            logo.classList.remove('inline-block');
+            logo.classList.add('hidden');
+        }
     }
+    
 });
