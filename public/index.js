04da9c69934677429beedc131162db675d679675
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2023-07-27 08:35:14 -0700

    Added Transition Fade in Fade Out to Mobile NavBar

--- public/index.js
+++ public/index.js
@@ -2,53 +2,21 @@ const burger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 const navbar = document.querySelector('header');
 const logo = document.querySelector('#logo');
+const mobileTitle = document.querySelector('#mobile-title');
 const offsetHeight = document.querySelector('header').offsetHeight;
 
-// burger.addEventListener('click', () => {
-//     if (menu.classList.contains('hidden')) {
-//         menu.classList.remove('hidden');
-//         navbar.classList.add('bg-white');
-//         logo.classList.remove('hidden');
-//         logo.classList.add('inline-block'); 
-//         navbar.classList.remove('text-primary-white');
-//         navbar.classList.add('text-primary-black');
-
-
-       
-//     } else {
-//         if (window.scrollY > offsetHeight) {
-//             navbar.classList.add('bg-white');
-//             navbar.classList.remove('text-primary-white');
-//             navbar.classList.add('text-primary-black');
-            
-//         } else {
-//             navbar.classList.remove('bg-white');
-//             navbar.classList.remove('text-primary-black');
-//             navbar.classList.add('text-primary-white');
-//             logo.classList.remove('inline-block');
-//             logo.classList.add('hidden');
-            
-            
-//         }
-//         menu.classList.add('hidden');
-         
-              
-        
-        
-//     }
-    
-    
-// });
 
 window.addEventListener('scroll', function() {
 
     if (window.scrollY > offsetHeight) {
         logo.classList.remove('hidden');
         logo.classList.add('inline-block');
-        navbar.classList.add('bg-white');
+        navbar.classList.add('bg-white','transition','duration-800');
         navbar.classList.remove('text-primary-white');
         navbar.classList.add('text-primary-black');
         navbar.classList.remove('transition', 'delay-800');
+        mobileTitle.classList.remove('hidden');
+
     } 
     else{
          if(menu.classList.contains('max-h-0')) {
@@ -57,6 +25,8 @@ window.addEventListener('scroll', function() {
             navbar.classList.add('text-primary-white');
             logo.classList.remove('inline-block');
             logo.classList.add('hidden');
+            mobileTitle.classList.add('hidden');
+            
          }
     }
     
@@ -65,27 +35,35 @@ window.addEventListener('scroll', function() {
 
 
 burger.addEventListener('click', () => {
+
     if (menu.classList.contains('max-h-0')) {
         menu.classList.remove('max-h-0');
         menu.classList.add('max-h-96');
         navbar.classList.add('bg-white'); 
         navbar.classList.add('text-primary-black');
         navbar.classList.remove('text-primary-white');
-        navbar.classList.remove('transition', 'delay-800');
+        navbar.classList.remove('transition', 'delay-800','duration-800');
+        
         logo.classList.remove('hidden');
         logo.classList.add('inline-block');
+        mobileTitle.classList.remove('hidden');
         
         
     } else {
         menu.classList.remove('max-h-96');
         menu.classList.add('max-h-0');
+        navbar.classList.add('transition', 'delay-800');
+        
+        
         if (window.scrollY <= offsetHeight) { // Add this condition
+            
             navbar.classList.remove('bg-white');
             navbar.classList.remove('text-primary-black');
             navbar.classList.add('text-primary-white');
-            navbar.classList.add('transition', 'delay-800');
+            // 
             logo.classList.add('hidden');
             logo.classList.remove('inline-block');
+            mobileTitle.classList.add('hidden');
 
         }
         
