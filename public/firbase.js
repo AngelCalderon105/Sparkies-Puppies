f83c68ab3eb675e8609a4a1192adde59b6f5291a
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2023-08-10 07:03:28 -0700

    firebase test

--- public/firbase.js
+++ public/firbase.js
@@ -1,36 +1,36 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js'; //retrieves function from file
-import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js'; //retrieves function from file
+// import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js'; //retrieves function from file
+// import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js'; //retrieves function from file
 
-const appSettings = {
-    databaseURL: "https://calderon-bulldogs-default-rtdb.firebaseio.com/"
-}
+// const appSettings = {
+//     databaseURL: "https://calderon-bulldogs-default-rtdb.firebaseio.com/"
+// }
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
+// const app = initializeApp(appSettings);
+// const database = getDatabase(app);
 
-const customersInDB = ref(database, "Customers");
+// const customersInDB = ref(database, "Customers");
 
 
 
-const inputName = document.querySelector('#input-name');
-const inputEmail = document.querySelector('#input-email');
-const inputPhone = document.querySelector('#input-phone');
-const submitButton = document.querySelector('#form-submit');
+// const inputName = document.querySelector('#input-name');
+// const inputEmail = document.querySelector('#input-email');
+// const inputPhone = document.querySelector('#input-phone');
+// const submitButton = document.querySelector('#form-submit');
 
-const formContainer = document.getElementById('form-container');
-const form = document.getElementById('reminderForm');
-const thankYouMessage = document.querySelector('#thankYouMessage');
+// const formContainer = document.getElementById('form-container');
+// const form = document.getElementById('reminderForm');
+// const thankYouMessage = document.querySelector('#thankYouMessage');
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    let nameValue = inputName.value;
-    let emailValue = inputEmail.value;
-    let phoneValue = inputPhone.value;
-    let userInfo = `${nameValue}, ${emailValue}, ${phoneValue}`;
-    push(customersInDB, userInfo);
-    inputName.value ='';
-    inputEmail.value ='';
-    inputPhone.value ='';
-    formContainer.classList.add('hidden');
-    thankYouMessage.classList.remove('hidden');
-});
+// form.addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     let nameValue = inputName.value;
+//     let emailValue = inputEmail.value;
+//     let phoneValue = inputPhone.value;
+//     let userInfo = `${nameValue}, ${emailValue}, ${phoneValue}`;
+//     push(customersInDB, userInfo);
+//     inputName.value ='';
+//     inputEmail.value ='';
+//     inputPhone.value ='';
+//     formContainer.classList.add('hidden');
+//     thankYouMessage.classList.remove('hidden');
+// });
